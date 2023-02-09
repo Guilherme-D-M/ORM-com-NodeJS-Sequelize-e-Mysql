a360df5d52c31b3c132af4cefa6f9bd42e1538fa
Guilherme-D-M <guidal1050@outlook.com>
2023-02-09 20:33:18 -0300

    Inserindo rotas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -2,7 +2,13 @@ const database = require('../models')
 
 class PessoaController {
     static async pegaTodasAsPessoas(req, res){
-        const todasAsPessoas = await database.Pessoas.findAll()
-        
+        try{
+            const todasAsPessoas = await database.Pessoas.findAll()
+            return res.status(200).json(todasAsPessoas)
+        }catch(error){
+            return res.status(500).json(error.message)
         }
+    }
 }
+
+module.exports = PessoaController
